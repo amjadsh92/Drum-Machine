1230f4a19f46195c9a7b45c923788fd3c4c6440e
amjadsh92 <amjadsharafeddien@gmail.com>
2025-03-18 17:24:41 +0200

    Center drum-machine

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
-const Hello = "Hello world"
+/* eslint-disable */
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./App.css";
+import { useState, useEffect } from "react";
+
+
+
+function App() {
+  
+  
+  
+  return (
+    <div className="p-1px bg-secondary vh-100 align-content-center">
+      <div id="drum-machine" className="w-600px h-300px mx-auto border border-4 border-warning bg-custom-gray ">
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App
