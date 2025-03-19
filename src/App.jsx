cca296b691286c63a60ea5c10b01f146c5ac4752
amjadsh92 <amjadsharafeddien@gmail.com>
2025-03-19 17:17:33 +0200

    Add power and display in display box

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./App.css";
 import { useState, useEffect } from "react";
 import "./styles/App.css"
 import "./styles/colors.css"
 import "./styles/sizes.css"
+import "./styles/fonts.css"
+
 
 
 
@@ -15,7 +16,7 @@ function App() {
   
   return (
     <div className="p-1px bg-secondary vh-100 align-content-center">
-      <div className="w-650px h-320px mx-auto border border-4 border-warning bg-custom-gray align-content-center">
+      <div className="d-flex gap-5 w-650px h-320px mx-auto border border-4 border-warning align-items-center bg-custom-gray">
           
         <div id="drum-machine" className="drum-machine">
           <button className="drum-pad">Q</button>
@@ -27,6 +28,23 @@ function App() {
           <button className="drum-pad">Z</button>
           <button className="drum-pad">X</button>
           <button className="drum-pad">C</button>
+        </div>
+
+        <div id="displayBox" className="displayBox d-flex gap-4 flex-column align-items-center">
+          <div className="power">
+          <p className="text-center fw-black fs-6 mb-0">Power</p>
+          <div className="power-switcher">
+            <button className="float-start cursor-pointer w-45 h-100 bg-primary"></button>
+          </div>
+
+          </div>
+          
+          <div className="display">
+
+          </div>
+          <div className="volume"></div>
+          <div className="bank"></div>
+          
         </div>
 
       </div>
