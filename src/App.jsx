e6d94331f725fe1a8f7bc4b2a61254fd27f52d7e
amjadsh92 <amjadsharafeddien@gmail.com>
2025-03-20 16:30:02 +0200

    Apply sound to all buttons

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,23 @@ import "./styles/fonts.css"
 
 function App() {
   
-  
+  const sounds = {
+    heater1: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3",
+    heater2: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3",
+    heater3: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3",
+    heater4: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3",
+    clap: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3",
+    openHH: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3",
+    kicknHat: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3",
+    kick: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3",
+    closedHH: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3"
+  }
   
   return (
     <div className="p-1px bg-secondary vh-100 align-content-center">
       <div className="d-flex gap-5 w-650px h-320px mx-auto border border-4 border-warning align-items-center bg-custom-gray">
           
-        <DrumMachine />
+        <DrumMachine sounds={sounds} />
         <DisplayBox />
       </div>
       
@@ -28,21 +38,24 @@ function App() {
 }
 
 
-function DrumMachine(){
+function DrumMachine({sounds}){
+
+  const { heater1, heater2, heater3, heater4, clap, openHH, kicknHat, kick, closedHH } = sounds;
 
 
   return(
 
     <div id="drum-machine" className="drum-machine">
-          <Drumpad />
-          <button className="drum-pad">W</button>
-          <button className="drum-pad">E</button>
-          <button className="drum-pad">A</button>
-          <button className="drum-pad">S</button>
-          <button className="drum-pad">D</button>
-          <button className="drum-pad">Z</button>
-          <button className="drum-pad">X</button>
-          <button className="drum-pad">C</button>
+          <Drumpad sound = {heater1} letter = "Q" />
+          <Drumpad sound = {heater2} letter = "W" />
+          <Drumpad sound = {heater3} letter = "E" />
+          <Drumpad sound = {heater4} letter = "A" />
+          <Drumpad sound = {clap} letter = "S" />
+          <Drumpad sound = {openHH} letter = "D" />
+          <Drumpad sound = {kicknHat} letter = "Z" />
+          <Drumpad sound = {kick} letter = "X" />
+          <Drumpad sound = {closedHH} letter = "C" />
+          
         </div>
   )
 
@@ -113,7 +126,7 @@ function Bank(){
 }
 
 
-function Drumpad(){
+function Drumpad({sound,letter}){
   
   const audioRef = useRef(null);
 
@@ -129,8 +142,8 @@ function Drumpad(){
 
   return (
     <>
-    <button className="drum-pad" onClick={playSound}>Q</button>
-    <audio ref={audioRef} id="audio-A" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"></audio>
+    <button id={letter} className="drum-pad" onClick={playSound} value={letter}>{letter}</button>
+    <audio ref={audioRef} id ={`audio-${letter}`}  src={sound}></audio>
     </>
   )
 }
