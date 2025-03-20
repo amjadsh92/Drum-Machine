d94f1975a99b5a37dea6528542d069c06ad96e5c
amjadsh92 <amjadsharafeddien@gmail.com>
2025-03-20 11:14:34 +0200

    Tranform the code into components

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,21 @@ function App() {
     <div className="p-1px bg-secondary vh-100 align-content-center">
       <div className="d-flex gap-5 w-650px h-320px mx-auto border border-4 border-warning align-items-center bg-custom-gray">
           
-        <div id="drum-machine" className="drum-machine">
+        <DrumMachine />
+        <DisplayBox />
+      </div>
+      
+    </div>
+  );
+}
+
+
+function DrumMachine(){
+
+
+  return(
+
+    <div id="drum-machine" className="drum-machine">
           <button className="drum-pad">Q</button>
           <button className="drum-pad">W</button>
           <button className="drum-pad">E</button>
@@ -29,35 +43,72 @@ function App() {
           <button className="drum-pad">X</button>
           <button className="drum-pad">C</button>
         </div>
+  )
 
-        <div id="displayBox" className="displayBox d-flex gap-3 flex-column align-items-center">
-          <div className="power">
+}
+
+
+function DisplayBox(){
+
+
+  return(
+
+    <div id="displayBox" className="displayBox d-flex gap-3 flex-column align-items-center">
+          <Power />
+          <Display />
+          <Volume />
+          <Bank />
+          
+        </div>
+  )
+}
+
+
+function Power(){
+
+  return(
+    <div className="power">
           <p className="text-center fw-black fs-6 mb-0">Power</p>
           <div className="power-switcher">
             <button className="float-start cursor-pointer w-45 h-100 bg-primary"></button>
           </div>
 
-          </div>
-          
-          <div className="display">
-
-          </div>
-          <input type="range" class="form-range" id="customRange" min="0" max="100" step="1" />
-          <div className="bank">
-
-          <p className="text-center fw-black fs-6 mb-0">Bank</p>
-          <div className="bank-switcher">
-            <button className="float-start cursor-pointer w-45 h-100 bg-primary"></button>
-          </div>
-
-          </div>
-          
-        </div>
-
-      </div>
-      
     </div>
-  );
+  )
+}
+
+function Display(){
+
+  return(
+    <div className="display">
+
+          </div>
+  )
+
+}
+
+function Volume(){
+  
+  return (
+    <input type="range" class="form-range" id="customRange" min="0" max="100" step="1" />
+  )
+}
+
+
+function Bank(){
+  return(
+
+    <div className="bank">
+
+    <p className="text-center fw-black fs-6 mb-0">Bank</p>
+    <div className="bank-switcher">
+      <button className="float-start cursor-pointer w-45 h-100 bg-primary"></button>
+    </div>
+
+    </div>
+    
+  
+  )
 }
 
 export default App
